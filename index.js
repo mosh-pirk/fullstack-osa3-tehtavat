0e67c338749c3fe9b7ec20759f4a65d0c13911d8
mosh-pirk <106318369+mosh-pirk@users.noreply.github.com>
2022-11-12 18:31:24 +0200

    tehtävä 3.1

---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456"
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
+    },
+    {
+        id: 3,
+        name: "Dan Abremov",
+        number: "12-43-234345"
+    },
+    {
+        id: 4,
+        name: "Mary Poppendick",
+        number: "39-23-6423122"
+    },
+    {
+        id: 5,
+        name: "Koodiin Tapahtuu muutoksia  ",
+        number: "40-25-6423911"
+    }
+]
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello Person App!</h1>')
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(x => x.id !== id)
+
+    response.status(204).end()
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(x => x.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
