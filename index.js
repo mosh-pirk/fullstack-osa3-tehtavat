a8b098e09068e666b655c642fec9113d4492e98a
mosh-pirk <106318369+mosh-pirk@users.noreply.github.com>
2022-11-20 20:47:01 +0200

    tehtävä 3.22

--- index.js
+++ index.js
@@ -1,83 +1,82 @@
 const express = require('express')
-const morgan = require('morgan');
+const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 const Persons = require('./models/perosn')
-const {request, response} = require("express");
 
 app.use(express.static('build'))
 app.use(cors())
 
-morgan.token('data', (req, res) => JSON.stringify(req.body))
-app.use(morgan(':method :url :res[content-length] - :response-time ms :data'));
+morgan.token('data', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :res[content-length] - :response-time ms :data'))
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello Person App!</h1>')
+  res.send('<h1>Hello Person App!</h1>')
 })
 
 // get all persons
 app.get('/api/persons', (req, res) => {
-    Persons.find({}).then(data => res.json(data))
+  Persons.find({}).then(data => res.json(data))
 })
 // add person
 app.post('/api/persons', (request, response, next) => {
-    const savedData = new Persons(returnPersonObject(request, response, false))
-    savedData.save()
-        .then(person => {
-           response.json(person).status(200)
-        })
-        .catch(error => next(error))
+  const savedData = new Persons(returnPersonObject(request, response, false))
+  savedData.save()
+    .then(person => {
+      response.json(person).status(200)
+    })
+    .catch(error => next(error))
 })
 // delete one person
 app.delete('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
-    Persons.findByIdAndDelete(id).then(result => {
-       response.status(204).end()
-    }).catch(error => next(error))
+  const id = request.params.id
+  Persons.findByIdAndDelete(id).then(() => {
+    response.status(204).end()
+  }).catch(error => next(error))
 
 })
 // get one person
 app.get('/api/persons/:id', (request, response, next) => {
-    Persons.findById(request.params.id)
-        .then(data => {
-            response.status(200).json(data)
-        }).catch(error => next(error))
+  Persons.findById(request.params.id)
+    .then(data => {
+      response.status(200).json(data)
+    }).catch(error => next(error))
 })
 // edit person
 app.put('/api/persons/:id', (request, response, next) => {
-    Persons.findByIdAndUpdate(request.params.id, returnPersonObject(request, response, true))
-        .then(data => {
-            response.status(200).json(data)
-        }).catch(error => next(error))
+  Persons.findByIdAndUpdate(request.params.id, returnPersonObject(request, response, true))
+    .then(data => {
+      response.status(200).json(data)
+    }).catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 
 const errorHandler = (error, request, response, next) => {
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } if (error.name === 'ValidationError') {
-        return response.status(400).send({ error: error})
-    }
-    next(error)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error })
+  }
+  next(error)
 }
 
 
 const returnPersonObject = (request, response, edit) => {
-    const body = request.body
-    return edit
-        ?
-        {number: body.number}
-        : {
-            name: body.name,
-            number: body.number,
-        }
+  const body = request.body
+  return edit
+    ?
+    { number: body.number }
+    : {
+      name: body.name,
+      number: body.number,
+    }
 }
 
 app.use(errorHandler)
@@ -85,5 +84,5 @@ app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- models/perosn.js
+++ models/perosn.js
@@ -1,40 +1,41 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+//@ts-ignore
 const dbUrl = process.env.MONGODB_URI
 
 mongoose.connect(dbUrl)
-    .then(result => {
-        console.log('connected to MongoDB', result.STATES.connected)
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    }).finally()
+  .then(result => {
+    console.log('connected to MongoDB', result.STATES.connected)
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  }).finally()
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: [true, 'User name required']
+  name: {
+    type: String,
+    minLength: 3,
+    required: [true, 'User name required']
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return (/\d{2}-\d{7}/.test(v) || /\d{3}-\d{8}/.test(v))
+      },
+      message: props => `${props.value} is not a valid phone number!`
     },
-    number: {
-        type: String,
-        validate: {
-            validator: function(v) {
-                return (/\d{2}-\d{7}/.test(v) || /\d{3}-\d{8}/.test(v));
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        },
-        required: [true, 'User phone number required']
-    }
+    required: [true, 'User phone number required']
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('persons', personSchema)
